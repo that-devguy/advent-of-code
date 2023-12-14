6d07fe2d721d24df198eda1c4c58ecf2ceacc239
that devguy <zkmutch@gmail.com>
2023-12-13 19:36:08 -0500

    day 2 - part 1 solution

--- 2023/day2/part1.js
+++ 2023/day2/part1.js
@@ -3,3 +3,31 @@ const filePath = "input.txt";
 const games = fs.readFileSync(filePath, "utf8").replace(/\r/g, "").split("\n");
 // console.log(games);
 
+const maxColors = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function parseGames(games) {
+  return games
+    .map((game) => {
+      return game
+        .split(": ")[1]
+        .split("; ")
+        .map((handful) => {
+          const pulls = handful.split(", ");
+          return pulls.every((pull) => {
+            const [count, color] = pull.split(" ");
+            //console.log(count, color);
+            return maxColors[color] >= Number(count);
+          });
+        })
+        .every((hand) => hand);
+    })
+    .reduce((s, result, i) => {
+      return result ? s + (i + 1) : s;
+    }, 0);
+}
+
+console.log(parseGames(games));
